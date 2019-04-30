b19cffd481c94efec56848c01a41180a187e2e2c
David BONFILS <dbonfils.ext@orange.com>
2019-04-30 10:37:07 +0200

    Enable & reconfigure powerline + Disable spaceline (too slow)

--- package.d/setup-powerline.el
+++ package.d/setup-powerline.el
@@ -9,10 +9,7 @@
 (use-package powerline
   :if window-system
   :config
-  (setq-default
-   powerline-default-separator 'utf-8
-   powerline-height 24
-   powerline-default-separator 'arrow))
+  (powerline-default-theme))
 
 (provide 'setup-powerline)
 
